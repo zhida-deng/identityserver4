4a9fa82c404d370b00caf7079caabd1c28845dec
dzd <601938862@qq.com>
2018-07-17 18:43:53 +0800

    0717

---
+++ OAuth/OAuth/Model/Config.cs
@@ -0,0 +1,51 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WXApi.Model
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+
+                new ApiResource("api","UsersApi")
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResourceResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(), //必须要添加，否则报无效的scope错误
+                new IdentityResources.Profile()
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+
+            return new List<Client>
+           {
+               new Client(){
+                   ClientId="client",//指定clientid
+                   AllowedGrantTypes=GrantTypes.ResourceOwnerPasswordAndClientCredentials,//授权模式
+                   ClientSecrets={
+                       new Secret("secrect".Sha256())
+                   },
+                   AllowedScopes={ "api",IdentityServerConstants.StandardScopes.OpenId, //必须要添加，否则报forbidden错误
+                  IdentityServerConstants.StandardScopes.Profile}//允许访问的api资源,指上面配置的api
+               }
+           };
+        }
+
+
+
+    }
+}
